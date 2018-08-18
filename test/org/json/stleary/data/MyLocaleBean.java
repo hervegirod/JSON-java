9cfb70ac42e691d664264cae4a40a4fa3b86967b
Herve <Herve@Herve-PC>
2018-08-18 14:45:44 +0200

    1.1

---
+++ test/org/json/stleary/data/MyLocaleBean.java
@@ -0,0 +1,14 @@
+package org.json.stleary.data;
+
+public class MyLocaleBean {
+   private final String id = "beanId";
+   private final String i = "beanI";
+
+   public String getId() {
+      return id;
+   }
+
+   public String getI() {
+      return i;
+   }
+}
